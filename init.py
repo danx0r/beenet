1eac66933b20f5f37989bf909264419ba9bd28bb
danx0r <dan.miller@eye0.com>
2020-02-08 00:31:56 -0800

    simplify

--- init.py
+++ init.py
@@ -14,16 +14,10 @@ os.system("pip install -r requirements.txt")
 if input("install tensorflow-gpu?").lower()[:1] == 'y':
     os.system("pip install -r tensorflow_gpu.txt")
 
-if input("install training data in user mode?").lower()[:1] == 'y':
-    os.system("pip install git+https://github.com/danx0r/beenet_data")
-
-elif input("install training data in developer mode?").lower()[:1] == 'y':
+if input("install training data?").lower()[:1] == 'y':
     os.system("git clone git@github.com:danx0r/beenet_data")
     os.system("pip install -e beenet_data")
 
-if input("install models in user mode?").lower()[:1] == 'y':
-    os.system("pip install git+ssh://hub@eye0.com:/home/hub/beenet/beenet_models#egg=beenet_coco_model")
-
-elif input("install models in developer mode?").lower()[:1] == 'y':
-    os.system("git clone git+ssh://hub@eye0.com:/home/hub/beenet/beenet_models#egg=beenet_coco_model")
+if input("install models in developer mode?").lower()[:1] == 'y':
+    os.system("git clone hub@eye0.com:/home/hub/beenet/beenet_models")
     os.system("pip install -e beenet_models")
