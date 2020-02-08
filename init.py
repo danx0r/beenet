023523df38e6b460c46c1b4db254c34062002352
danx0r <dan.miller@eye0.com>
2020-02-07 20:10:46 -0800

    reorder init sequence

--- init.py
+++ init.py
@@ -14,16 +14,16 @@ os.system("pip install -r requirements.txt")
 if input("install tensorflow-gpu?").lower()[:1] == 'y':
     os.system("pip install -r tensorflow_gpu.txt")
 
-if input("install training data in developer mode?").lower()[:1] == 'y':
-    os.system("git clone git@github.com:danx0r/beenet_data")
-    os.system("pip install -e beenet_data")
-
 if input("install training data in user mode?").lower()[:1] == 'y':
     os.system("pip install git+https://github.com/danx0r/beenet_data")
 
-if input("install models in developer mode?").lower()[:1] == 'y':
-    os.system("git clone ~/beenet_models")
-    os.system("pip install -e beenet_models")
+elif input("install training data in developer mode?").lower()[:1] == 'y':
+    os.system("git clone git@github.com:danx0r/beenet_data")
+    os.system("pip install -e beenet_data")
 
 if input("install models in user mode?").lower()[:1] == 'y':
     os.system("pip install git+file:///beenet_models")
+
+elif input("install models in developer mode?").lower()[:1] == 'y':
+    os.system("git clone ~/beenet_models")
+    os.system("pip install -e beenet_models")
