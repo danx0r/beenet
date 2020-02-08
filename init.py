5b1d9399445dc00f606f5b02c4b7611502efca05
danx0r <dan.miller@eye0.com>
2020-02-07 17:22:16 -0800

    check for venv & py3

--- init.py
+++ init.py
@@ -1,4 +1,12 @@
-import os
+import os, sys
+
+if not hasattr(sys, 'real_prefix'):
+    print ("Strongly suggest you run this in a virtualenv environment")
+    exit()
+
+if sys.version[0] != "3":
+    print ("Python3 pleez!")
+    exit()
 
 print ("installing requirements")
 os.system("pip install -r requirements.txt")
@@ -6,9 +14,9 @@ os.system("pip install -r requirements.txt")
 if input("install tensorflow-gpu?").lower()[:1] == 'y':
     os.system("pip install -r tensorflow_gpu.txt")
 
-if input("install beenet_data in developer mode?").lower()[:1] == 'y':
+if input("install training data in developer mode?").lower()[:1] == 'y':
     os.system("git clone git@github.com:danx0r/beenet_data")
-    os.system("pip install beenet_data")
+    os.system("pip install -e beenet_data")
 
-if input("install beenet_data in udser mode?").lower()[:1] == 'y':
+if input("install training data in user mode?").lower()[:1] == 'y':
     os.system("pip install git+https://github.com/danx0r/beenet_data")
