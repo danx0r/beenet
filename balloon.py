98f4f68d527698b80538ab3a7fc801cbedec0b62
danx0r <dan.miller@eye0.com>
2020-02-08 17:01:22 -0800

    expected output

--- balloon.py
+++ balloon.py
@@ -1,32 +1,3 @@
-"""
-Mask R-CNN
-Train on the toy Balloon dataset and implement color splash effect.
-
-Copyright (c) 2018 Matterport, Inc.
-Licensed under the MIT License (see LICENSE for details)
-Written by Waleed Abdulla
-
-------------------------------------------------------------
-
-Usage: import the module (see Jupyter notebooks for examples), or run from
-       the command line as such:
-
-    # Train a new model starting from pre-trained COCO weights
-    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=coco
-
-    # Resume training a model that you had trained earlier
-    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=last
-
-    # Train a new model starting from ImageNet weights
-    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=imagenet
-
-    # Apply color splash to an image
-    python3 balloon.py splash --weights=/path/to/weights/file.h5 --image=<URL or path to file>
-
-    # Apply color splash to video using the last weights you trained
-    python3 balloon.py splash --weights=last --video=<URL or path to file>
-"""
-
 import os
 import time
 import sys
@@ -34,21 +5,16 @@ import json
 import datetime
 import numpy as np
 import skimage.draw
-
-# Root directory of the project
-ROOT_DIR = os.path.abspath("../../")
-
-# Import Mask RCNN
-sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn.config import Config
 from mrcnn import model as modellib, utils
+import base_coco_model
 
 # Path to trained weights file
-COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+COCO_WEIGHTS_PATH = os.path.join(base_coco_model.datapath(), "mask_rcnn_coco.h5")
 
 # Directory to save logs and model checkpoints, if not provided
 # through the command line argument --logs
-DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+DEFAULT_LOGS_DIR = "logs"
 
 ############################################################
 #  Configurations
--- detect.py
+++ detect.py
@@ -1,10 +1,13 @@
 import os
-import openfootballoon
+import balloon_test_model
 
-cmd = """
-python3 balloon.py splash --weights %smask_rcnn_balloon.h5 --image party.jpg 
-diff -s party_splashed.png splash_expected.png
-""" % openfootballoon.datapath
+cmd = "python3 balloon.py splash --weights %smask_rcnn_balloon_ep1.h5 --image party.jpg" % balloon_test_model.datapath()
 
+print (cmd)
+os.system(cmd)
+
+cmd = "diff -s party_splashed.png party_expected.png"
+
+print ("TEST PASSES UNLESS diff fails:")
 print (cmd)
 os.system(cmd)
