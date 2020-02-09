63dddca52f4c049603610367ec04ff727a01243e
danx0r <ubuntu@ip-172-31-19-194.us-west-2.compute.internal>
2020-02-09 05:40:45 +0000

    just doit

--- train.py
+++ train.py
@@ -4,4 +4,4 @@ import balloon_test_data, base_coco_model
 cmd = "python balloon.py train --weights %sbase_coco_model.h5 --dataset %s" % (base_coco_model.datapath(), balloon_test_data.datapath())
 
 print (cmd)
-#os.system(cmd)
+os.system(cmd)
