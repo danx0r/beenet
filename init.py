be3375c8916e32e94485bff233f82433cf14e935
danx0r <dan.miller@eye0.com>
2020-02-09 21:23:29 -0800

    init.py

--- init.py
+++ init.py
@@ -1,22 +1,17 @@
 import os
 
 print ("installing requirements")
-os.system("pip install -r requirements1.txt")
-os.system("pip install -r requirements2.txt")
+os.system("pip install -r requirements.txt")
+# os.system("pip install -r requirements2.txt")
 
 if input("install tensorflow-gpu?").lower()[:1] == 'y':
-    os.system("pip install -r tensorflow_gpu.txt")
+    os.system("pip install -r requirements-gpu.txt")
 
-if input("install available trained model: openfootballoon (245 Mbyte)?").lower()[:1] == 'y':
-    os.system("pip install -r models.txt")
-
-if input("install available datasets via_annotated_1 & test_balloons (60 Mbyte)?").lower()[:1] == 'y':
-    os.system("pip install -r datasets.txt")
-
-if input("run training (break out with ctl-C)?").lower()[:1] == 'y':
-    os.chdir("samples/balloon")
+if input("run balloon training test?").lower()[:1] == 'y':
+    os.chdir("tests")
     os.system("python train.py")
+    os.chdir("..")
 
-if input("run pre-trained splash test?").lower()[:1] == 'y':
-    os.chdir("samples/balloon")
+if input("run pre-trained balloon detection test?").lower()[:1] == 'y':
+    os.chdir("tests")
     os.system("python detect.py")
